e91966cbe7c9e9eb585d204de9ef99887d0e0463
Kobakat <54965702+Kobakat@users.noreply.github.com>
2020-02-18 02:11:00 -0600

    Now moves multiple floors at a time

--- Elevator.c
+++ Elevator.c
@@ -2,30 +2,73 @@
 #include <stdlib.h>
 
 
-void ChangeFloor(int start, int final, int floors[])
+void ChangeFloor(int start, int size, int floors[], int * list)
 {
-	
-	int difference = final - start;
-	int i = start;
-	
-	//decrease if change in floors is negative
-	if (difference < 0) 
+	for(int i = 0; i < size; i++)
 	{
-		for(int i = start; i > final; i--) 
+		int difference = *(list + i) - start;
+		
+		//decrease if change in floors is negative
+		if (difference < 0) 
 		{
-			printf("%d, ", floors[i-2]);
+			for(int k = start; k > *(list + i); k--) 
+			{
+				printf("%d, ", floors[k-2]);
+			}
+			printf("Ding!\n");
 		}	
-	}
 	
-	//increase if change in floors is positive
-	else
-	{
-		for(int i = start; i < final; i++)
+		//increase if change in floors is positive
+		else if (difference > 0)
 		{
-			printf("%d, ", floors[i]);
+			for(int k = start; k < *(list + i); k++)
+			{
+				printf("%d, ", floors[k]);
+			}
+			printf("Ding!\n");
+		}
+
+		//If no net floor change occurs
+		else
+		{
+			printf("Oops! You're already on that floor!\n");
+		}
+		
+		start = *(list + i);
+	}	
+}
+
+
+//Organizes the desired floors input by the user into a dynamically sized array
+int * AssignFloors(int numOfFloors) 
+{
+	int * list;
+
+	//Clear/allocate memory for a list array
+	list = calloc(numOfFloors, sizeof(int));
+	
+	for(int j = 0; j < numOfFloors; j++) 
+	{
+		int chosenFloor = 0;
+		
+		printf("Enter floor number #%d\n", j+1);
+		scanf("%d", &chosenFloor);
+
+		//incorrect floor chosen
+		if(chosenFloor > 15 || chosenFloor < 0) 
+		{
+			printf("That is not a valid floor! Choose between 1-15.\n");
+			j--;
+		}
+		
+		//proper floor chosen
+		else
+		{	
+			*(list + j) = chosenFloor;
 		}
 	}
-	printf("Ding!\n");
+
+	return list;
 }
 
 int main() 
@@ -36,31 +79,25 @@ int main()
 	
 	do 
 	{
-		int chosenFloor = 1;
+		int numOfFloors = 0;
 		
 		printf("You are currently on floor %d. The top floor is 15.\n", currentFloor);
-		printf("Which floor would you like to go to? ");
-		
-		scanf("%d", &chosenFloor);
+		printf("How many floors would you like to go to? ");
 
-		//incorrect floor chosen
-		if(chosenFloor > 15 || chosenFloor < 0) 
-		{
-			printf("That is not a valid floor! Choose between 1-15.\n");
-		}
+		//Used to determine the size of the array
+		scanf("%d", &numOfFloors);
 
-		//same floor chosen
-		else if(chosenFloor == currentFloor) 
-		{
-			printf("You're already on that floor!\n");
-		}
-		
-		//proper floor chosen
-		else
-		{	
-			ChangeFloor(currentFloor, chosenFloor, floors);
-			currentFloor = chosenFloor;
-		}
+		//floorList points to an array of size numOfFloors
+		int * floorList = AssignFloors(numOfFloors);
+
+		printf("\nHere we go!\n\n");
+
+		//Text showing the changing of floors
+		ChangeFloor(currentFloor, numOfFloors, floors, floorList);
+
+		currentFloor = *(floorList + numOfFloors - 1);
+
+		free(floorList);
 
 		//ride again if input is 'y'
 		printf("Would you like to change floors? Enter y if yes or any other key if not.\n");
@@ -72,5 +109,4 @@ int main()
 	} while(again == 'y');
 
 	return 0;
-
 }
